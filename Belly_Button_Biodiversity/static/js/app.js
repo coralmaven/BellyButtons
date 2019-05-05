50bb961f78f684e89e214c50f1276c1252148278
Suchita <coralmaven@yahoo.com>
2019-05-05 15:04:49 -0500

    First version of charts

--- Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -1,32 +1,122 @@
-function buildMetadata(sample) {
+async function buildMetadata(sample) {
 
-  // @TODO: Complete the following function that builds the metadata panel
-
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
-
-    // Use `.html("") to clear any existing metadata
-
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+  let sampleMetadata = d3.select("#sample-metadata");
+  sampleMetadata.html("");
+  metadata = await d3.json("/metadata/".concat(sample));
+  for ( var key in metadata){
+    sampleMetadata.append("tr").text(key.concat(": ").concat(metadata[key].toString()));
+  };
+  
+  // Build the Gauge Chart
+  buildGauge(metadata.WFREQ);
 }
 
-function buildCharts(sample) {
+async function buildGauge(frequency){
+console.log(frequency);
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+let data = [{
+  values: [1,1,1,1,1,1,1,1,1,9],
+  rotation: 90,
+  //text: ['8-9','7-8','6-7','5-6','4-5','3-4','2-3','1-2','0-1'],
+  text: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
+  textinfo: 'text',
+  textposition: 'inside',
+  marker: {
+    colors: [ 'rgba(110, 154, 22, .5)',
+              'rgba(14, 127, 0, .5)',
+              'rgba(110, 154, 22, .5)',
+              'rgba(14, 127, 0, .5)',
+              'rgba(110, 154, 22, .5)',
+              'rgba(14, 127, 0, .5)',
+              'rgba(110, 154, 22, .5)',
+              'rgba(14, 127, 0, .5)',
+              'rgba(110, 154, 22, .5)',
+              'rgba(0, 0, 0, 0)'
+    ]
+  },
+  hoverinfo: false,
+  hole: .5,
+  type: 'pie',
+  showlegend: false,
+  direction: "clockwise"
+}];
 
-    // @TODO: Build a Bubble Chart using the sample data
+let layout = {
+  title: '<b>Belly Button Washing Frequency</b><br>Scrubs per Week',
+  height: 600,
+  width: 600,
+  xaxis: {
+    zeroline: false,
+    showticklabels: false,
+    showgrid: false,
+    fixedrange: true,
+    range: [-1, 1]
+  },
 
-    // @TODO: Build a Pie Chart
+  yaxis: {
+    zeroline: false,
+    showticklabels: false,
+    showgrid: false,
+    fixedrange: true,
+    range: [-1, 1]
+  }
+};
+
+Plotly.newPlot('gauge', data, layout);
+s = d3.select("#gauge.pielayer").selectAll(".slice")
+    .filter(d3.matcher("1-2"));
+console.log(s.select("path.surface"))
+ 
+// add the gauge pointer
+// use the frequency value to figure out the slice to point to
+// find the center of the guage arccircle.
+// The draw a svg line from the center of the arc to the slicetext.
+// or to the path of the slice.
+
+}
+
+async function buildCharts(sample) {
+
+  // Using `d3.json` to fetch the sample data for the plots
+  sampledata = await d3.json("/samples/".concat(sample));
+  metadata = await d3.json("/metadata/".concat(sample));
+
+  // // Bubble Chart for the sample data
+  var trace = {
+    x: sampledata.otu_ids,
+    y: sampledata.sample_values,
+    text: sampledata.otd_labels,
+    mode: 'markers',
+    marker: {
+      color: sampledata.otu_ids,
+      size: sampledata.sample_values
+    }
+  };
+  
+  var data = [trace];
+  
+  var layout = {
+    title: 'Bubble Chart',
+    showlegend: true,
+    height: 600,
+    width: 600
+  };
+  
+  Plotly.newPlot('bubble', data, layout);
+
+  //Pie Chart
+  data = [{
+    "labels": sampledata.otu_ids.slice(0,9),
+    "values": sampledata.sample_values.slice(0,9),
+    "type": "pie"}]
+  var layout = {title : "Pie chart"}
+  Plotly.plot("pie", data, layout);
+
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
 }
 
-function init() {
+async function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
 
